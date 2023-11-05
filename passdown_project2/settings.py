8cfa1174539d55a218d8a056519e692eea5af082
Jonathan <jhamilton2394@gmail.com>
2023-11-05 14:13:09 -0800

    dev mode false

--- passdown_project2/settings.py
+++ passdown_project2/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "True") == "True"
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 
 # Application definition
